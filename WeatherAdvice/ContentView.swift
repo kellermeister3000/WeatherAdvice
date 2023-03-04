45b93a4088dc5b014fb2fd8c9e0fd806976c6fda
Philip Keller <philipjoelkeller@gmail.com>
2023-03-03 22:36:33 -0800

    Weather Advice App - Boston College Prof. Gallagher

--- WeatherAdvice/ContentView.swift
+++ WeatherAdvice/ContentView.swift
@@ -8,19 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var temp = ""
+    @State private var imageName = ""
+    @State private var adviceMessage = ""
+    @FocusState private var textFieldIsFocused: Bool
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Weather Advice")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundColor(.blue)
+            
+            Spacer()
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+            
+            Spacer()
+            
+            Text(adviceMessage)
+                .font(.largeTitle)
+                .frame(height: 80)
+                .minimumScaleFactor(0.5)
+            
+            Spacer()
+            
+            HStack {
+                Text("What is the temp?")
+                
+                TextField("", text: $temp)
+                    .textFieldStyle(.roundedBorder)
+                    .frame(width: 65)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(.gray, lineWidth: 2)
+                    }
+                    .keyboardType(.numberPad)
+                    .focused($textFieldIsFocused)
+            }
+            .font(.title)
+            
+            Button {
+                giveAdvice()
+                textFieldIsFocused = false
+            } label: {
+                Text("Get Advice!")
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.blue)
+            .font(.title2)
+            .disabled(temp.isEmpty)
+            
         }
         .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    func giveAdvice() {
+        guard let temperature = Int(temp) else {return}
+        switch temperature {
+        case 76...:
+            adviceMessage = "It's hot out!"
+            imageName = "hot"
+        case 63..<76:
+            adviceMessage = "Nice and mild."
+            imageName = "mild"
+        case 45..<63:
+            adviceMessage = "Nice and mild."
+            imageName = "cool"
+        case 33..<45:
+            adviceMessage = "Nice and mild."
+            imageName = "cold"
+        default:
+            adviceMessage = "Bundle up, it's freezing!"
+            imageName = "freezing"
+            
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
