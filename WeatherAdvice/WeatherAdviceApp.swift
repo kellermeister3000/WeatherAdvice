47afbaa77ee184ca1d713a5c3c7cf85c3ad4457d
Philip Keller <philipjoelkeller@gmail.com>
2023-03-03 22:15:40 -0800

    Initial Commit

---
+++ WeatherAdvice/WeatherAdviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAdviceApp.swift
+//  WeatherAdvice
+//
+//  Created by Philip Keller on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAdviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
